0bad5937716973eb3de465592767cc871a79471c
Carlos <cavillo@gmail.com>
2020-02-11 23:01:49 -0500

    Fixing bin

--- bin/cli.js
+++ bin/cli.js
@@ -3,48 +3,50 @@ const cp = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
+let PROJECT_NAME;
+let PROJECT_DIR;
+
 const log = (...args) => console.log('|-> ', ...args);
+const logError = (...args) => console.error('Error: ', ...args);
 
 const cleanup = () => {
-  console.log('Cleaning up...');
-  const projectDir = path.join(__dirname, '..', projectName);
-  if (fs.existsSync(projectDir)) {
-    cp.execSync(`rm -rf ${projectDir}`);
+  if (fs.existsSync(PROJECT_DIR)) {
+    cp.execSync(`rm -rf ${PROJECT_DIR}`);
   }
 };
 
 const handleError = (error) => {
-  console.error('Error: An error was encountered while executing');
-  console.error(error);
+  logError('An error was encountered while executing', error.message);
   cleanup();
-  console.log('Exiting...');
   process.exit(1);
 };
 
-const createProject = () => {
+const init = () => {
   const args = process.argv.slice(2);
   if (args.length === 0) {
-    console.error('Error: PROJECT_NAME required');
+    logError('PROJECT_NAME required');
     process.exit();
   }
-  const projectName = args[0];
-  const projectDir = path.join(__dirname, '..', projectName);
+  PROJECT_NAME = args[0];
+  PROJECT_DIR = path.join(process.cwd(), PROJECT_NAME);
 
   // check if a folder already exists with this name and fail gracefully
-  if (fs.existsSync(projectDir)) {
-    console.error('Error: PROJECT_NAME already exists');
+  if (fs.existsSync(PROJECT_DIR)) {
+    logError('PROJECT_NAME already exists');
     process.exit();
   }
+};
 
+const createProject = () => {
   try {
-    console.log('|-> Creating project directory...');
-    cp.execSync(`git clone https://github.com/cavillo/ts-base.git ${projectName} --quiet`);
+    log('Creating project directory...');
+    cp.execSync(`git clone https://github.com/cavillo/ts-base.git ${PROJECT_NAME} --quiet`);
 
     // updating package.json
-    console.log('|-> Setting up project...');
-    const packageJson = path.join(projectDir, 'package.json');
+    log('Setting up project...');
+    const packageJson = path.join(PROJECT_DIR, 'package.json');
     const json = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
-    json.name = projectName;
+    json.name = PROJECT_NAME;
     json.version = "1.0.0";
     json.author = "";
     json.license = "";
@@ -52,34 +54,37 @@ const createProject = () => {
     fs.writeFileSync(packageJson, JSON.stringify(json, null, 2), 'utf8');
 
     // removing git directory and package lock
-    const packageLockJson = path.join(projectDir, 'package-lock.json');
-    const binDir = path.join(projectDir, 'bin');
-    const gitDir = path.join(projectDir, '.git');
+    const packageLockJson = path.join(PROJECT_DIR, 'package-lock.json');
+    const binDir = path.join(PROJECT_DIR, 'bin');
+    const gitDir = path.join(PROJECT_DIR, '.git');
     cp.execSync(`rm -rf ${packageLockJson}`);
     cp.execSync(`rm -rf ${binDir}`);
     cp.execSync(`rm -rf ${gitDir}`);
 
     // installing dependendies
-    console.log('|-> Installing dependendies...');
-    cp.execSync(`npm i --prefix ${projectDir} --silent`);
+    log('Installing dependendies...');
+    cp.execSync(`npm i --prefix ${PROJECT_DIR} --silent`);
 
-    console.log(`|-> Project [${projectName}] created succesfully! ✅`);
-    console.log('|-> Happy ts coding! 💪\n');
+    log(`Project [${PROJECT_NAME}] created succesfully! ✅`);
+    log('Happy ts coding! 💪');
+    log('');
 
-    printHelp(projectName);
+    printHelp(PROJECT_NAME);
   } catch (error) {
     handleError(error);
   }
 };
 
-const printHelp = (projectName) => {
-  console.log(`\tcd ${projectName}\n`);
-  console.log('\tnpm run start-ts    run TS');
-  console.log('\tnpm run lint        lint');
-  console.log('\tnpm run test        run tests');
-  console.log('\tnpm run build       build JS');
-  console.log('\tnpm start           run builded JS\n');
+const printHelp = (PROJECT_NAME) => {
+  log(`\tcd ${PROJECT_NAME}`);
+  log('');
+  log('\tnpm run start-ts    run TS');
+  log('\tnpm run lint        lint');
+  log('\tnpm run test        run tests');
+  log('\tnpm run build       build JS');
+  log('\tnpm start           run builded JS\n');
 };
 
 // Create project
+init();
 createProject();
